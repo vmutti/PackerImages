cad8fd015a0d079f3b88d3a13612bb4895d4e61e
Joshua Makinen <makinj64@gmail.com>
2024-12-07 00:18:16 -0500

    good amount of changes I think, just pushing

--- platforms/virtualbox-iso/provision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-debian_frontend=noninteractive
-sudo apt -y update;
-sudo apt -y upgrade;
-sudo apt -y install dkms build-essential linux-headers-$(uname -r);
-(mkdir -p /tmp/VBoxGuestAdditions && sudo mount "$HOME/VBoxGuestAdditions.iso" /tmp/VBoxGuestAdditions -o loop) || true;
-sudo /tmp/VBoxGuestAdditions/VBoxLinuxAdditions.run || true;
-sudo /opt/VBoxGuestAdditions-*/init/vboxadd setup || exit 0;
